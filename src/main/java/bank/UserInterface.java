870323d57cc26698e65a78e1a90e5347c2edd848
Mauricio Chimento <jmau.chimento@gmail.com>
2017-12-08 13:30:36 +0100

    minor upd

--- src/main/java/bank/UserInterface.java
+++ src/main/java/bank/UserInterface.java
@@ -18,10 +18,7 @@ public class UserInterface {
      * Gets the user associated to the interface.
      *
      * @return the user associated to the interface
-     */
-  /*@ public normal_behaviour
-    @ ensures \result == u ;
-    @ */	
+     */	
     public User getUser() {
       return null; 
     }
@@ -30,12 +27,7 @@ public class UserInterface {
      * Instantiates a new user interface.
      *
      * @param str the name of the interface
-     */
-  /*@ public normal_behaviour
-    @ requires str != null ;
-    @ ensures name == str && u == null ;
-    @ assignable \everything ;
-    @ */	
+     */	
     UserInterface(String str){
     }	
 	
@@ -44,35 +36,13 @@ public class UserInterface {
      *
      * @param userName the user name
      * @param password the password
-     */
-  /*@ public normal_behaviour
-    @ requires u == null ;
-    @ ensures u != null ;
-    @ assignable u ;
-    @
-    @ also
-    @
-    @ public normal_behaviour
-    @ requires u != null ;
-    @ assignable \nothing ;
-    @ */	
+     */	
     public void login (String userName, int password) {
     }
 
     /**
      * Logs out a user from the system.
-     */
-  /*@ public normal_behaviour
-    @ requires u != null ;
-    @ ensures u == null ;
-    @ assignable u ;
-    @
-    @ also
-    @
-    @ public normal_behaviour
-    @ requires u == null ;
-    @ assignable \nothing ;
-    @ */	
+     */	
     public void logout () {
     }
 
@@ -80,24 +50,7 @@ public class UserInterface {
      * Deposits the money in the user's account.
      *
      * @param money the amount of money to deposit in the user's account
-     */
-  /*@ public normal_behaviour
-    @ requires money > 0 && u != null ;
-    @ ensures u.getAccount().getBalance() == \old(u).getAccount().getBalance() + money ;
-    @ assignable u ;
-    @
-    @ also
-    @
-    @ public normal_behaviour
-    @ requires money <= 0 && u != null ;
-    @ assignable \nothing ;
-    @
-    @ also
-    @
-    @ public normal_behaviour
-    @ requires u == null ;
-    @ assignable \nothing ;
-    @ */	
+     */	
     public void deposit(int money){
     }
 	
@@ -105,25 +58,7 @@ public class UserInterface {
      * Withdraws the money from the user's account.
      *
      * @param money the amount of money to withdraw from the user's account
-     */
-  /*@ public normal_behaviour
-    @ requires u != null ;
-    @ requires money > 0 && (u.getAccount().getBalance() - money >= 0);
-    @ ensures u.getAccount().getBalance() == \old(u).getAccount().getBalance() - money ;
-    @ assignable u ;
-    @
-    @ also
-    @
-    @ public normal_behaviour
-    @ requires money <= 0 && u != null ;
-    @ assignable \nothing ;
-    @
-    @ also
-    @
-    @ public normal_behaviour
-    @ requires u == null ;
-    @ assignable \nothing ;
-    @ */	
+     */	
     public void withdraw(int money){
     }
 }
