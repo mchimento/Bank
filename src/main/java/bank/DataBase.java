faf98cd7fbb1ca8d7f3a4eaaa76c602364a210b3
Mauricio Chimento <jmau.chimento@gmail.com>
2017-12-08 13:01:31 +0100

    Minor upd database files

--- src/main/java/bank/DataBase.java
+++ src/main/java/bank/DataBase.java
@@ -13,15 +13,19 @@ public final class DataBase {
 
     /** Sample user Checho. */
     private static User Checho = new User("checho","Mauricio", "Chimento", 1);
+    private static Account accChecho = new Account();
     
     /** Sample user Wolfgang. */
     private static User Wolfgang = new User("supervisor","Wolfgang", "Ahrendt", 3);
+    private static Account accWolfgang = new Account();
     
     /** Sample user Gerardo. */
     private static User Gerardo = new User("cosupervisor", "Gerardo", "Schneider", 4); 
+    private static Account accGerardo = new Account();
     
     /** Array representing the database. */
     static User[] db = {Checho, Wolfgang, Gerardo};
+    static Account[] accs = {accChecho,accWolfgang,accGerardo};
     
     /**
      * Searches for the user <tt>userName</tt> in the database. If the password is wrong, or the user
@@ -38,6 +42,7 @@ public final class DataBase {
         while (i < capacity){			
             if (db[i].getUserName().equals(userName) && (db[i].getPassword() == password)) {
                u = db[i];
+               u.setAccount(accs[i]);
                break;
             } else {
                  i++;
