22c3219bca30f66fd23682b9772e66b07460c676
Mauricio Chimento <jmau.chimento@gmail.com>
2018-01-16 17:46:35 +0100

    Main implemented

--- src/main/java/bank/Main.java
+++ src/main/java/bank/Main.java
@@ -3,6 +3,8 @@
  */
 package bank;
 
+import java.util.Scanner;
+
 /**
  * The Class Main.
  */
@@ -15,6 +17,65 @@ public class Main {
     * @throws Exception the exception
     */
    public static void main(String[] args) throws Exception {
+       UserInterface f = new UserInterface("Bank");
+       String amount; int aux;
 
+       Scanner in = new Scanner (System.in);
+       int exit = 0;
+
+       System.out.println("Welcome to the ATM.");
+
+       while (exit == 0) {
+           System.out.println("\nChoose an action to perform:");
+
+           String inputLine = in.next();
+           if (f.getUser() == null){
+               switch (inputLine) {
+                   case "login":
+                       System.out.print("Enter your username: ");
+                       String userName = in.next();
+
+                       System.out.print("\nEnter your password: ");
+                       String password = in.next();
+
+                       f.login(userName, Integer.parseInt(password));
+                       break;
+
+                   case "quit":
+                       System.out.println("\nLeaving the program.");
+                       exit = 1;
+                       break;
+
+                   default:
+                       System.out.println("Wrong action.");
+                       break;
+               }
+           } else {
+               switch (inputLine) {
+                   case "deposit":
+                       System.out.print("Enter the amount of money to deposit: ");
+                       amount = in.next();
+                       aux = Integer.parseInt(amount);
+                       f.deposit(aux);
+                       break;
+                   case "withdraw":
+                       System.out.print("Enter the amount of money to deposit: ");
+                       amount = in.next();
+                       aux = Integer.parseInt(amount);
+                       f.deposit(aux);
+                       break;
+                   case "balance":
+                       System.out.printf("\nYour balance is %d.\n", f.getUser().getAccount().getBalance());
+                       break;
+                   case "logout":
+                       f.logout();
+                       exit = 1;
+                       break;
+                   default: System.out.println("Wrong action.");
+                       break;
+               }
+           }
+       }
+       in.close();
    }
 }
